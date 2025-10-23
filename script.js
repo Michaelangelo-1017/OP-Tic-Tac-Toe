9f81c2b8e30eb339c3308e91a6ab1791a108d247
Michaelangelo <jagbodyke01@gmail.com>
2025-10-23 20:53:15 +0100

    Add DOM functionality to display and run game
    This commit will update the html,css and script
    files to add the functionality for the DOM so we
    can run the game to an extent on the DOM.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//
 function createBoard(){
     return {
         board: ["","","","","","","","",""],
@@ -12,6 +13,21 @@ function createBoard(){
         },
         reset(){
             this.board = ["","","","","","","","",""];
+        },
+        displayBoard(){
+            this.board.forEach((square,index)=>{
+                document.querySelector('.board').innerHTML += `<button class='tic-square' id='square-${index}'>${square}</button>`
+            })
+            currentBoard.updateWalkthrough(`${playGame.players[0]}'s turn`)
+        },
+        updateBoard(){
+            document.querySelector('.board').innerHTML = '';
+            this.board.forEach((square,index)=>{
+                document.querySelector('.board').innerHTML += `<button class='tic-square' id='square-${index}'>${square}</button>`;
+            })
+        },
+        updateWalkthrough(text){
+            document.getElementById('walkthrough').innerText = text;
         }
     }
 }
@@ -21,20 +37,46 @@ function createPlayer(name){
 }
 
 function gameControls(){
+    const winningSets = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[2,4,6]];
     return {
-        players: [player1,player2],
+        players: ['Player 1','Player 2'],
         marks: ['X','O'],
         currentPlayerIndex : 0,
-        playTurn(){
-            if(currentBoard.placeMark(this.setCurrentMark,this.generateIndex)){
-                this.switchPlayer()
+        canStillPlay : true,
+        winner : "",
+        checkWin(){
+            const markIndexArr = [];
+            currentBoard.board.forEach((space,index)=>{
+                if(space === this.setCurrentMark){
+                    markIndexArr.push(index)
+                }
+            }); 
+            const checkerArr = winningSets.map((winArr)=> winArr.every((val)=>markIndexArr.includes(val)));
+            if(checkerArr.includes(true)){
+                currentBoard.updateWalkthrough(`${this.setCurrentPlayer} with the mark : ${this.setCurrentMark} has won the game.`);
+                this.canStillPlay = false;
+                this.winner = this.setCurrentMark;
+            }
+        },
+        playTurn(ticIndex){
+            if(this.canStillPlay){
+                if(currentBoard.placeMark(this.setCurrentMark,ticIndex)){
+                this.switchPlayer();
+                this.checkWin();
+                currentBoard.updateBoard();
+            }
+                else{
+                    this.playTurn();
+                }
             }
             else{
-                this.playTurn();
+                currentBoard.updateWalkthrough(`${this.winner} has won already`)
             }
         },
         switchPlayer(){
+            
             this.currentPlayerIndex = this.currentPlayerIndex === 0 ? 1 : 0;
+            currentBoard.updateWalkthrough(`${this.setCurrentPlayer}'s turn`); 
         },
         get setCurrentPlayer(){
             return this.players[this.currentPlayerIndex];
@@ -53,16 +95,20 @@ function gameControls(){
     
 }
 
-const player1 = createPlayer('Mike');
-const player2 = createPlayer('Duke');
 const currentBoard = createBoard();
 const playGame = gameControls();
-console.log(player1);
-console.log(player2);
+currentBoard.displayBoard()
 console.log(currentBoard);
 console.log(playGame);
-for(let i=1; i<=9; i++){
-    console.log(`It is ${playGame.setCurrentPlayer.name}'s turn, he played ${playGame.setCurrentMark} in ${playGame.generateIndex}`);
-    playGame.playTurn();
-}
+
+const boardCont = document.querySelector('.board');
+
+boardCont.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('tic-square')){
+        const ticId = e.target.id;
+        playGame.playTurn(ticId[ticId.length-1]);
+        console.log('Btn clicked')
+    }
+})
+
 console.log(currentBoard.board)
